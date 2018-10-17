a1295dcf3d3cf9cd493cf8d88b2b5ac266059a63
Abiodun Owoseje <abiodunowoseje@gmail.com>
2018-10-17 10:52:36 -0400

    update files

--- models/app.py
+++ models/app.py
@@ -1,12 +1,16 @@
 from database import Database
-from models.blog import Blog
-from models.post import Post
+from menu import Menu
+#from models.blog import Blog
+#from models.post import Post
 
 __author__ = 'Abiodun'
 
 Database.initialize()
 
-blog = Blog(title="The Act",
+menu = Menu()
+
+menu.run_menu()
+'''blog = Blog(title="The Act",
             author="Johnny Bravo",
             description="The Act of doing right")
 
@@ -16,14 +20,14 @@ blog.save_to_mongo()
 
 from_database = Blog.get_from_mongo(id)
 
-'''post2 = Post(blog_id="1234",
+post2 = Post(blog_id="1234",
              title="Things Fall Apart",
              author="Chinua Achebe",
              content="Nobel prize book")
-'''
 
 print(blog.get_post())
 
-#blog.save_to_mongo()
+blog.save_to_mongo()
+'''
 
 
